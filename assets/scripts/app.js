6291d18cc46d30dac3fc45e849c22fe405e92fba
Frank-Pechar <93831849+Frank-Pechar@users.noreply.github.com>
2023-06-04 19:48:00 -0600

    Minor changes and added comments to code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,11 +4,11 @@
 // Select global dom objects using various methods
 const addMovieModal = document.getElementById('add-modal');
 // const addMovieModal = document.querySelector('#add-modal');
-// const addMovieModal = document.body.children[1];
+// const addMovieModal = document.body.children[3];
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
 const backdrop = document.getElementById('backdrop');
-// const backdrop = document.body.firstElementChild;
+// const backdrop = document.body.children[2];
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
@@ -63,15 +63,21 @@ const startDeleteMovieHandler = (movieId) => {
   deleteMovieModal.classList.add('visible');
   toggleBackdrop();
 
+  // create pointers to model buttons
   const cancelDeletionButton = deleteMovieModal.querySelector('.btn--passive');
   let confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
 
-  confirmDeletionButton.replaceWith(confirmDeletionButton.cloneNode(true));
-
-  confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
-
+  // remove old event listener if it exists
   cancelDeletionButton.removeEventListener('click', closeMovieDeletionModal);
 
+  // remove old event listener if it exists for element that has an event listener bound handler function by:
+  // 1) recreate DOM button element contents by creating a copy using a deep clone of the element
+  // 2) replaceWith will replace DOM button element contents with cloned button element
+  // 3) recreate pointer to the new cloned copied DOM element for new event listener to be attached
+  confirmDeletionButton.replaceWith(confirmDeletionButton.cloneNode(true));
+  confirmDeletionButton = deleteMovieModal.querySelector('.btn--danger');
+
+  // add new event listeners to delete movie modal buttons
   cancelDeletionButton.addEventListener('click', closeMovieDeletionModal);
   confirmDeletionButton.addEventListener(
     'click',
@@ -81,6 +87,7 @@ const startDeleteMovieHandler = (movieId) => {
 
 // render new movie to UI list
 const renderNewMovieElement = (id, title, imageUrl, rating) => {
+  // create new li element for the new movie
   const newMovieElement = document.createElement('li');
   newMovieElement.className = 'movie-element';
   newMovieElement.innerHTML = `
@@ -92,10 +99,12 @@ const renderNewMovieElement = (id, title, imageUrl, rating) => {
       <p>${rating}/5 stars</p>
     </div>
   `;
+  // add delete movie event listener handler and pass id argument
   newMovieElement.addEventListener(
     'click',
     startDeleteMovieHandler.bind(null, id)
   );
+  // add new movie li element to ul list
   const listRoot = document.getElementById('movie-list');
   listRoot.append(newMovieElement);
 };
@@ -105,7 +114,6 @@ const closeMovieModal = () => {
 };
 
 const showMovieModal = () => {
-  // function() {}
   addMovieModal.classList.add('visible');
   toggleBackdrop();
 };
@@ -122,12 +130,13 @@ const cancelAddMovieHandler = () => {
   clearMovieInput();
 };
 
-// add new movie to array and call functons to render to UI
+// add new movie to array and call functions to render to UI
 const addMovieHandler = () => {
   const titleValue = userInputs[0].value;
   const imageUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
 
+  // validate input
   if (
     titleValue.trim() === '' ||
     imageUrlValue.trim() === '' ||
@@ -135,10 +144,11 @@ const addMovieHandler = () => {
     +ratingValue < 1 ||
     +ratingValue > 5
   ) {
-    alert('Please enter valid values (rating between 1 and 5).');
+    alert('Please enter valid non-blank values also rating between 1 and 5.');
     return;
   }
 
+  // create movie object
   const newMovie = {
     id: Math.random().toString(),
     title: titleValue,
@@ -146,11 +156,12 @@ const addMovieHandler = () => {
     rating: ratingValue,
   };
 
+  // add movie object to movie list array
   movies.push(newMovie);
-  console.log(movies);
   closeMovieModal();
   toggleBackdrop();
   clearMovieInput();
+  // call function to render new movie to list
   renderNewMovieElement(
     newMovie.id,
     newMovie.title,
@@ -166,6 +177,7 @@ const backdropClickHandler = () => {
   clearMovieInput();
 };
 
+// Initialization routine - adding event listener handlers to buttons
 startAddMovieButton.addEventListener('click', showMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
 cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
